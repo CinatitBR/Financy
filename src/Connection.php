01a22a7bdd9a920b73c5151f62c78800d2c0a8fc
CinatitBR <cinatit.igor.santos@gmail.com>
2020-11-20 03:13:47 -0300

    refactor code

--- src/Connection.php
+++ src/Connection.php
@@ -1,16 +1,20 @@
 <?php
 
-abstract class Connection {
+class Connection {
   private static $connection;
 
   public static function getConnection() {
-    $host = 'localhost';
-    $username = 'root';
-    $password = '';
-    $db_name = 'pais';
 
-    // Create database connection
-    self::$connection = new mysqli($host, $username, $password, $db_name);
+    // Check if $connection doesn't exists
+    if(!isset(self::$connection)) {
+      $host = 'localhost';
+      $username = 'root';
+      $password = '';
+      $db_name = 'pais';
+  
+      // Create database connection
+      self::$connection = new mysqli($host, $username, $password, $db_name);
+    }
 
     return self::$connection;
   }
--- src/models/CadastroModel.php
+++ src/models/CadastroModel.php
@@ -2,17 +2,21 @@
 
 class CadastroModel {
 
-  public static function teste() {
-    // Get database connection
-    $connection = Connection::getConnection();
+  private $connection;
 
+  function __construct() {
+    // Get database connection
+    $this->connection = Connection::getConnection();
+  }
+
+  public static function teste() {
     $sql = '
       SELECT nm_cidade
       FROM cidade
       WHERE cd_cidade = 1
     ';
 
-    $res = $db->query($sql);
+    $res = $connection->query($sql);
     $row = $res->fetch_assoc();
     var_dump($row);
   }
