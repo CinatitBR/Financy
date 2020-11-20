901ea95fb3559d36026d52a6baabe3c951ac0900
CinatitBR <cinatit.igor.santos@gmail.com>
2020-11-20 18:50:06 -0300

    update config

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+  // Database connection
+  $host = 'localhost';
+  $user = 'root';
+  $password = '';
+  $dbName = 'gestor_financeiro';
+
+  // APPROOT (app root folder)
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // URLROOT (root folder)
+  define('URLROOT', 'pw1/Financy');
+
+  // SITENAME
+  define('SITENAME', 'Financy');
