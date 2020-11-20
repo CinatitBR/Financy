9bb8247c47cfe31fc955b01716c4c7440034d121
CinatitBR <cinatit.igor.santos@gmail.com>
2020-11-20 00:00:55 -0300

    update Core.php

--- src/core/Core.php
+++ src/core/Core.php
@@ -1,9 +1,25 @@
 <?php
+require('./src/controllers/Home.php');
+
 class Core {
   public function start($get_url) {
-    $page = ucfirst($get_url['page'] . 'Controller');
+    // Get controller name from the query string in the URL
+    $controller_name = ucfirst($get_url['page'] . 'Controller');
+    $action = 'index';
 
-    echo $page;
+    // Verify whether controller exists
+    if (!class_exists($controller)) {
+      echo "Página não encontrada";
+      return;
+    }
+
+    // Creates controller object
+    $controller_obj = new $controller;
+
+    // Executes $action method
+    $controller_ob->{$action}();
+
+    // call_user_func_array(array(new $controller, $acao))
 
   }
 }
