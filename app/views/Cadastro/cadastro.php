9d1bce7a096b97e7c36300605b07bc89b38b14ca
CinatitBR <cinatit.igor.santos@gmail.com>
2020-11-21 15:17:00 -0300

    update views

--- app/views/Cadastro/cadastro.php
+++ app/views/Cadastro/cadastro.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-  
+<head>
+
   <?php require APPROOT . '/views/templates/head.php' ?>
+  
+</head>
+<body>
 
-  <body>
+  <?php require APPROOT . '/views/templates/header.php' ?>
 
-    <?php require APPROOT . '/views/templates/header.php' ?>
+  <div class="wrapper">
 
-    <div class="wrapper">
-
-      <form method="POST" action="<?php echo './cadastro/register' ?>" class="form">
-        <div class="form-info__wrapper">
-          <p class="form-info">
-            Já tem uma conta? <a href="#">Entrar</a>
-          </p>
+    <form method="POST" action="<?php echo './cadastro/register' ?>" class="form">
+      <div class="form-info__wrapper">
+        <p class="form-info">
+          Já tem uma conta? <a href="#">Entrar</a>
+        </p>
+      </div>
+    
+      <h1 class="form-title">Cadastre-se.</h1>
+    
+      <div class="field-list">
+        <div class="item">
+          <label for="name">Como você gostaria de ser chamado?</label>
+          <input type="text" name="username" maxlength="100" id="name" required />
         </div>
-      
-        <h1 class="form-title">Cadastre-se.</h1>
-      
-        <div class="field-list">
-          <div class="item">
-            <label for="name">Como você gostaria de ser chamado?</label>
-            <input type="text" name="username" maxlength="100" id="name" required />
-          </div>
-      
-          <div class="item">
-            <label for="email">Nos diga qual o seu melhor Email:</label>
-            <input type="email" name="email" maxlength="254" id="email" required />
-          </div>
-      
-          <div class="item">
-            <label for="password">Digite uma senha forte para sua conta:</label>
-            <input type="password" name="password" class="form-control" id="password" maxlength="255" required />
-          </div>
-      
-        <button type="submit" class="form-button">Avançar</button>
-      </form>
+    
+        <div class="item">
+          <label for="email">Nos diga qual o seu melhor Email:</label>
+          <input type="email" name="email" maxlength="254" id="email" required />
+        </div>
+    
+        <div class="item">
+          <label for="password">Digite uma senha forte para sua conta:</label>
+          <input type="password" name="password" class="form-control" id="password" maxlength="255" required />
+        </div>
+    
+      <button type="submit" class="form-button">Avançar</button>
+    </form>
 
-    </div>
+  </div>
 
-  </body>
+</body>
 </html>
--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-  
+<head>
   <?php require APPROOT . '/views/templates/head.php' ?>
+</head>
+<body>
 
-  <body>
+  <?php APPROOT . '/views/templates/header.php' ?>
 
-    <?php APPROOT . '/views/templates/header.php' ?>
+  <div class="wrapper">
 
-    <div class="wrapper">
+    <form class="form">
+      <div class="form-info-wrapper">
+        <p class="form-info">
+          Ainda não tem uma conta? <a href="#">Cadastrar</a>
+        </p>
+      </div>
 
-      <form class="form">
-        <div class="form-info-wrapper">
-          <p class="form-info">
-            Ainda não tem uma conta? <a href="#">Cadastrar</a>
-          </p>
+      <h1 class="form-title">Login</h1>
+
+      <div class="field-list">
+
+        <div class="item">
+          <label for="email">Digite seu email:</label>
+          <input type="email" id="email" maxlength="254" required />
         </div>
-  
-        <h1 class="form-title">Login</h1>
-  
-        <div class="field-list">
-  
-          <div class="item">
-            <label for="email">Digite seu email:</label>
-            <input type="email" id="email" maxlength="254" required />
-          </div>
-  
-          <div class="item">
-            <label for="password">Digite sua senha:</label>
-            <input type="password" class="form-control" id="password" maxlength="255" required />
-          </div>
-  
-        </div>
-  
-        <button type="submit" class="form-button">Avançar</button>
-      </form>
-      
-    </div>
 
-  </body>
+        <div class="item">
+          <label for="password">Digite sua senha:</label>
+          <input type="password" class="form-control" id="password" maxlength="255" required />
+        </div>
+
+      </div>
+
+      <button type="submit" class="form-button">Avançar</button>
+    </form>
+    
+  </div>
+
+</body>
 </html>
