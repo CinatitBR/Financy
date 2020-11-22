045930c5caca8e2336c948a49993ecc5212df111
CinatitBR <cinatit.igor.santos@gmail.com>
2020-11-22 16:19:57 -0300

    Update name validaton regex

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -26,15 +26,17 @@
         $data['email'] = trim($_POST['email']);
         $data['password'] = trim($_POST['password']); 
 
+        $data['username'] = preg_replace("/\s+/", ' ', $data['username']); // Replace multiple spaces with a single space
+
         // Define characters that can be used in the username
-        $nameValidation = "/^[a-zA-z0-9]*$/";
+        $nameValidation = "/^[\wÀ-ÿ\s]*$/";
 
         // Validate username
         if(empty($data['username'])) {
           $data['usernameError'] = "Por favor, digite o nome que você quer ser chamado.";
         } 
         elseif(!preg_match($nameValidation, $data['username'])) {
-          $data['usernameError'] = "O nome só pode conter letras e números.";
+          $data['usernameError'] = "O nome só pode conter letras, números, acentos e espaços.";
         }
 
         // Validate email
@@ -44,7 +46,7 @@
         elseif(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) { // If the email doesn't have the correct format
           $data['emailError'] = "Por favor, digite o email corretamente.";
         }
-        else{ // Verify is the email is already taken
+        else { // Verify is the email is already taken
           if($this->userModel->findUserByEmail($data['email'])) {
             $data['emailError'] = "Este email já está cadastrado. Por favor, insira outro email ou entre em sua conta.";
           }
@@ -56,7 +58,6 @@
           // Hash the password
           $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
 
-          // echo var_dump($this->userModel->register($data));
           // Register user using model function
           if($this->userModel->register($data)) {
             // Redirect to login page
