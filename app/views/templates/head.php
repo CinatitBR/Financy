244b978b4e962fbc24339a5f82cd106874068874
CinatitBR <cinatit.igor.santos@gmail.com>
2020-12-01 19:05:35 -0300

    Update head.php

--- app/views/templates/head.php
+++ app/views/templates/head.php
@@ -1,4 +1,3 @@
-<?php echo header ('Content-type: text/html; charset=utf-8'); ?>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <!-- Bootstrap CSS -->
