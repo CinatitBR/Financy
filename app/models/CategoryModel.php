171124e7c158fda7f248ec3e168de7f84e49f83c
CinatitBR <cinatit.igor.santos@gmail.com>
2020-12-01 20:09:39 -0300

    Add new parameter to function

--- app/models/CategoryModel.php
+++ app/models/CategoryModel.php
@@ -1,11 +1,12 @@
 <?php 
   class CategoryModel extends Database {
 
-    public function getCategories($user_id) {
+    public function getCategories($flow, $user_id) {
       $sql = "
         SELECT category_id, category 
         FROM category
         WHERE user_id = $user_id
+        AND flow = '$flow'
       ";
 
       $result = $this->query($sql);
