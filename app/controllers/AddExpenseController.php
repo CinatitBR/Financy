a0f0dd9149516ac6d02509926c708fc029d1844c
CinatitBR <cinatit.igor.santos@gmail.com>
2020-12-01 15:32:14 -0300

    Get data from categoryModel

--- app/controllers/AddExpenseController.php
+++ app/controllers/AddExpenseController.php
@@ -1,18 +1,24 @@
 <?php 
   class AddExpenseController extends Controller {
     private $accountModel;
+    private $categoryModel;
 
     public function __construct() {
       $this->accountModel = $this->model("AccountModel");
+      $this->categoryModel = $this->model("CategoryModel");
     }
 
     public function index() {
       $data = [
         "title" => "Adicionar despesa",
-        "accounts" => []
+        "accounts" => [],
+        "categories" => []
       ];
 
-      $data["accounts"] = $this->accountModel->getAccounts($_SESSION['user_id']);
+      $user_id = $_SESSION['user_id'];
+
+      $data["accounts"] = $this->accountModel->getAccounts($user_id);
+      $data["categories"] = $this->categoryModel->getCategories($user_id);
 
       $this->view("addExpense", $data);
     }
