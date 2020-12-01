80961b2b909e70728638fe56a3fab04aa4d71bfc
CinatitBR <cinatit.igor.santos@gmail.com>
2020-12-01 20:09:57 -0300

    Update PaymentModel

--- app/models/PaymentModel.php
+++ app/models/PaymentModel.php
@@ -10,9 +10,29 @@
       $status_id
     ) {
 
+      $sql = "";
+      $result = "";
+
+      // If the transaction is completed
+      if ($status_id == 2) {
+
+        $sql = "
+          UPDATE account
+          SET balance = balance - $value
+          WHERE account_id = $account_id
+        ";
+
+        $result = $this->query($sql);
+
+        // If the query was not successfull
+        if (!$result) {
+          return false;
+        }
+      }
+
       $sql = "
         INSERT INTO payment (value, description, user_id, account_id, category_id, status_id)
-        VALUES ($value, $description, $user_id, $account_id, $category_id, $status_id);
+        VALUES ($value, '$description', $user_id, $account_id, $category_id, $status_id);
       ";
 
       $result = $this->query($sql);
