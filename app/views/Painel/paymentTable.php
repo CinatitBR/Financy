892016da0f92c15dd923928e2de58ec1a6340464
CinatitBR <cinatit.igor.santos@gmail.com>
2020-12-12 13:13:01 -0300

    Update style

--- app/views/Painel/paymentTable.php
+++ app/views/Painel/paymentTable.php
@@ -2,10 +2,11 @@
   <table class="payment-table table table-hover table-dark">
     <thead>
       <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">Valor</th>
+        <th scope="col">Conta</th>
+        <th scope="col">Categoria</th>
+        <th scope="col">Data</th>
+        <th scope="col">Status</th>
       </tr>
     </thead>
     <tbody>
@@ -14,17 +15,21 @@
         <td>Mark</td>
         <td>Otto</td>
         <td>@mdo</td>
+        <td>20/2015</td>
       </tr>
       <tr>
         <th scope="row">2</th>
         <td>Jacob</td>
         <td>Thornton</td>
         <td>@fat</td>
+        <td>20/2015</td>
       </tr>
       <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
+        <th scope="row">R$ 540,00</th>
+        <td>R$ 540,00</td>
+        <td>teste</td>
         <td>@twitter</td>
+        <td>20/2015</td>
       </tr>
     </tbody>
   </table>
